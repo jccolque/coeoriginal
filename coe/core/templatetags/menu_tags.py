8281d22123763a89e269d7539a7609a91b72bc1d
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-03-18 18:17:47 -0300

    Se mejoro el sistema de menu

--- coe/core/templatetags/menu_tags.py
+++ coe/core/templatetags/menu_tags.py
@@ -11,7 +11,9 @@ def ct_opciones(usuario):
     listado = []
     for app in CoreConfig.ADMIN_MENU:
         permiso_name = 'operadores.menu_' + app[1]
-        print(permiso_name)
-        if usuario.has_perm(permiso_name):
+        if False:
+            if usuario.has_perm(permiso_name):
+                listado.append(app)
+        else:
             listado.append(app)
     return listado
