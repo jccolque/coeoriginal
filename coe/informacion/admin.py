d9a9b08263944b68c6f3ae8f37434f3b460328c8
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-03-19 14:19:22 -0300

    Se quitaron restricciones en admin

--- coe/informacion/admin.py
+++ coe/informacion/admin.py
@@ -17,22 +17,16 @@ class SituacionInline(admin.TabularInline):
     model = Situacion
     fk_name = 'individuo'
     extra = 0
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 class AtributoInline(admin.TabularInline):
     model = Atributo
     fk_name = 'individuo'
     extra = 0
-    def has_delete_permission(self, request, obj=None):
-        return False   
 
 class SintomaInline(admin.TabularInline):
     model = Sintoma
     fk_name = 'individuo'
     extra = 0
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 class IndividuoInline(admin.TabularInline):
     model = Individuo
@@ -44,8 +38,6 @@ class ArchivoAdmin(admin.ModelAdmin):
     model = Archivo
     search_fields = ['nombre',]
     list_filter = ['tipo', ]
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 class EnfermedadAdmin(admin.ModelAdmin):
     model = Individuo
@@ -62,15 +54,11 @@ class IndividuoAdmin(admin.ModelAdmin):
     search_fields = ['nombres', 'apellidos', 'num_doc', ]
     list_filter = ['nacionalidad']
     inlines = [SituacionInline, AtributoInline, SintomaInline, ]
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 class VehiculoAdmin(admin.ModelAdmin):
     model = Vehiculo
     search_fields = ['identificacion',]
     list_filter = ['tipo']
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 # Register your models here.
 admin.site.register(TipoSintoma)
