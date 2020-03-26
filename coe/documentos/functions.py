749225d96498c56a981ef982d00bf95ff727f9c7
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-03-25 22:17:29 -0300

    Solucionamos orden de documentos en pagina publica

--- coe/documentos/functions.py
+++ coe/documentos/functions.py
@@ -8,6 +8,7 @@ def ver_publicadas(limit=None):
     versiones = []
     documentos = Documento.objects.filter(publico=True)
     documentos = documentos.prefetch_related('versiones')
+    documentos = documentos.order_by('-fecha')
     for doc in documentos:
         version = doc.ultima_version()
         if version:
