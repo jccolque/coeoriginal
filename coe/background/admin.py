b7f99850839ebc4385622097524e04338e235674
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-03-30 13:37:21 -0300

    Eliminamos parameters de task por que el admin se muere xD

--- coe/background/admin.py
+++ coe/background/admin.py
@@ -22,14 +22,14 @@ dec_priority.short_description = "priority -= 1"
 #Definimos nuestros admins
 class TaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
-    search_fields = ['task_name', 'task_params', ]
-    list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
+    search_fields = ['task_name',]
+    list_display = ['task_name', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
     actions = [inc_priority, dec_priority]
 
 class CompletedTaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
-    search_fields = ['task_name', 'task_params', ]
-    list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
+    search_fields = ['task_name',]
+    list_display = ['task_name', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
 
 class Progress_LinksAdmin(admin.ModelAdmin):
     search_fields = ['tarea', ]
