14b0e88a35b5549a3920318a08d7e642920f10f2
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-03-30 20:29:19 -0300

    No se puede hacer background de instancias

--- coe/informacion/utilidades/populate_actuales.py
+++ coe/informacion/utilidades/populate_actuales.py
@@ -1,35 +1,32 @@
-
-from background_task import background
-#Imports del proyecto
-from background.tasks import crear_progress_link
-#Imports de la app
+#Imports de django
+from django.utils import timezone
+#imports de la app
 from informacion.models import Individuo
 from informacion.models import Domicilio, Situacion
 
-@background(schedule=1)
-def masive_update(individuos_to_update):
-    print("Lanzamos el Bulk Update")
-    Individuo.objects.bulk_update(individuos_to_update, ['domicilio_actual', 'situacion_actual'])
-
 def actuales():
-    tarea = crear_progress_link('CARGAR ACTUALES')
-    individuos_to_update = []
     print("Iniciamos la actualizacion de:", Individuo.objects.count(), "Registros")
+    individuos_to_update = []
+    print("Generando dict de Domicilios. Inicio:", timezone.now())
     domicilios = {d.individuo.id:d for d in Domicilio.objects.all().order_by('fecha').select_related('individuo')}
-    print("Generamos dict de Domicilios")
+    print("Generando dict de Situaciones. Inicio:", timezone.now())
     situaciones = {s.individuo.id:s for s in Situacion.objects.all().order_by('fecha').select_related('individuo')}
-    print("Genramos dict de Situaciones")
+    print("Empezamos el bucle Gigante:", timezone.now())
     indice = 0
     for i in Individuo.objects.all():
         indice += 1
-        if (indice % 10000) == 0:
-            print("Se procesaron:", indice, "Registros")
-            #MAndamos bulk update
-            masive_update(individuos_to_update, queue=tarea)
-            #Blanqueamos para el proximo batch
+        if (indice % 15000) == 0:
+            print("Se procesaron:", indice, "Registros. Time:", timezone.now())
+            Individuo.objects.bulk_update(individuos_to_update, ['domicilio_actual', 'situacion_actual'])
+            print("Terminamos el Bulk Update. Time:", timezone.now())
             individuos_to_update = []
         if i.pk in domicilios:
             i.domicilio_actual = domicilios[i.pk]
         if i.pk in situaciones:
             i.situacion_actual = situaciones[i.pk]
         individuos_to_update.append(i)
+    #MAndamos bulk update
+    print("Se procesaron:", indice, "Registros")
+    Individuo.objects.bulk_update(individuos_to_update, ['domicilio_actual', 'situacion_actual'])
+    print("Terminamos el Bulk Update. Time:", timezone.now())
+    
