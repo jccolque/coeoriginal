6cb5836be6f07e8a4122f028c01d2201c4d2dacd
Juan Manuel Lopez <juan.lopez@valtira.net>
2020-04-07 08:44:44 -0300

    test

--- AppCovid19/lib/main.dart
+++ AppCovid19/lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:covidjujuy_app/ui/formulario.dart';
 import 'package:covidjujuy_app/ui/temperatura.dart';
 import 'package:flutter/material.dart';
@@ -10,8 +12,56 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'package:geolocation/geolocation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:background_fetch/background_fetch.dart';
+import 'package:cron/cron.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
-void main() => runApp(MyApp());
+const EVENTS_KEY = "fetch_events";
+
+/// This "Headless Task" is run when app is terminated.
+void backgroundFetchHeadlessTask(String taskId) async {
+  print("[BackgroundFetch] Headless event received: $taskId");
+  print('2222222*************** esta enviando datos!!!');
+  DateTime timestamp = DateTime.now();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  // Read fetch_events from SharedPreferences
+  List<String> events = [];
+  String json = prefs.getString(EVENTS_KEY);
+  if (json != null) {
+    events = jsonDecode(json).cast<String>();
+  }
+  // Add new event.
+  events.insert(0, "$taskId@$timestamp [Headless]");
+  // Persist fetch events in SharedPreferences
+  prefs.setString(EVENTS_KEY, jsonEncode(events));
+
+  BackgroundFetch.finish(taskId);
+
+  if (taskId == 'flutter_background_fetch') {
+    BackgroundFetch.scheduleTask(TaskConfig(
+        taskId: "com.transistorsoft.customtask",
+        delay: 5000,
+        periodic: false,
+        forceAlarmManager: true,
+        stopOnTerminate: false,
+        enableHeadless: true
+    ));
+  }
+}
+
+void main() {
+  // Enable integration testing with the Flutter Driver extension.
+  // See https://flutter.io/testing/ for more info.
+  runApp(new MyApp());
+
+  // Register to receive BackgroundFetch events after app is terminated.
+  // Requires {stopOnTerminate: false, enableHeadless: true}
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -49,6 +99,10 @@ class _MyLoginPageState extends State<MyLoginPage> {
   StreamSubscription<LocationResult> streamSubscription;
   bool trackLocation = false;
 
+  bool _enabled = true;
+  int _status = 0;
+  List<String> _events = [];
+
   @override
   void initState() {
     _launchFirstTermCondDialogConfirmation();
@@ -58,6 +112,182 @@ class _MyLoginPageState extends State<MyLoginPage> {
     checkGps();
 
     trackLocation = false;
+
+    initPlatformState();
+
+//    var cron = Cron();
+//    cron.schedule(Schedule.parse('*/1 * * * *'), () async {
+//      print('33333--------------every one minutes');
+//      getLocations();
+//    });
+  }
+
+  Future<void> initPlatformState() async {
+    // Load persisted fetch events from SharedPreferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String json = prefs.getString(EVENTS_KEY);
+    if (json != null) {
+      setState(() {
+        _events = jsonDecode(json).cast<String>();
+      });
+    }
+
+    // Configure BackgroundFetch.
+    BackgroundFetch.configure(BackgroundFetchConfig(
+      minimumFetchInterval: 15,
+      forceAlarmManager: false,
+      stopOnTerminate: false,
+      startOnBoot: true,
+      enableHeadless: true,
+      requiresBatteryNotLow: false,
+      requiresCharging: false,
+      requiresStorageNotLow: false,
+      requiresDeviceIdle: false,
+      requiredNetworkType: NetworkType.NONE,
+    ), _onBackgroundFetch).then((int status) {
+      print('[BackgroundFetch] configure success: $status');
+      setState(() {
+        _status = status;
+      });
+
+    }).catchError((e) {
+      print('[BackgroundFetch] configure ERROR: $e');
+      setState(() {
+        _status = e;
+      });
+    });
+
+    // Schedule a "one-shot" custom-task in 10000ms.
+    // These are fairly reliable on Android (particularly with forceAlarmManager) but not iOS,
+    // where device must be powered (and delay will be throttled by the OS).
+    BackgroundFetch.scheduleTask(TaskConfig(
+        taskId: "com.transistorsoft.customtask",
+        delay: 10000,
+        periodic: false,
+        forceAlarmManager: true,
+        stopOnTerminate: false,
+        enableHeadless: true
+    ));
+
+    // Optionally query the current BackgroundFetch status.
+    int status = await BackgroundFetch.status;
+    setState(() {
+      _status = status;
+    });
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+  }
+
+  void _onBackgroundFetch(String taskId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    DateTime timestamp = new DateTime.now();
+    // This is the fetch-event callback.
+
+    getLocations();
+    setState(() {
+      _events.insert(0, "$taskId@${timestamp.toString()}");
+    });
+    // Persist fetch events in SharedPreferences
+    prefs.setString(EVENTS_KEY, jsonEncode(_events));
+
+    if (taskId == "flutter_background_fetch") {
+      // Schedule a one-shot task when fetch event received (for testing).
+      BackgroundFetch.scheduleTask(TaskConfig(
+          taskId: "com.transistorsoft.customtask",
+          delay: 5000,
+          periodic: false,
+          forceAlarmManager: true,
+          stopOnTerminate: false,
+          enableHeadless: true
+      ));
+    }
+
+    // IMPORTANT:  You must signal completion of your fetch task or the OS can punish your app
+    // for taking too long in the background.
+    BackgroundFetch.finish(taskId);
+  }
+
+  void _onClickEnable(enabled) {
+    setState(() {
+      _enabled = enabled;
+    });
+    if (enabled) {
+      BackgroundFetch.start().then((int status) {
+        print('[BackgroundFetch] start success: $status');
+      }).catchError((e) {
+        print('[BackgroundFetch] start FAILURE: $e');
+      });
+    } else {
+      BackgroundFetch.stop().then((int status) {
+        print('[BackgroundFetch] stop success: $status');
+      });
+    }
+  }
+
+  void _onClickStatus() async {
+    int status = await BackgroundFetch.status;
+    print('[BackgroundFetch] status: $status');
+    setState(() {
+      _status = status;
+    });
+  }
+
+  void _onClickClear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(EVENTS_KEY);
+    setState(() {
+      _events = [];
+    });
+  }
+
+  Widget buildCron(BuildContext context) {
+    const EMPTY_TEXT = Center(child: Text('Waiting for fetch events.  Simulate one.\n [Android] \$ ./scripts/simulate-fetch\n [iOS] XCode->Debug->Simulate Background Fetch'));
+
+    return new Container(
+      width: 600.0,
+      height: 400.0,
+      child: new Scaffold(
+        appBar: new AppBar(
+            title: const Text('BackgroundFetch Example', style: TextStyle(color: Colors.black)),
+            backgroundColor: Colors.amberAccent,
+            brightness: Brightness.light,
+            actions: <Widget>[
+              Switch(value: _enabled, onChanged: _onClickEnable),
+            ]
+        ),
+        body: (_events.isEmpty) ? EMPTY_TEXT : Container(
+          child: new ListView.builder(
+              itemCount: _events.length,
+              itemBuilder: (BuildContext context, int index) {
+                List<String> event = _events[index].split("@");
+                return InputDecorator(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 5.0, top: 5.0, bottom: 5.0),
+                        labelStyle: TextStyle(color: Colors.blue, fontSize: 20.0),
+                        labelText: "[${event[0].toString()}]"
+                    ),
+                    child: new Text(event[1], style: TextStyle(color: Colors.black, fontSize: 16.0))
+                );
+              }
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            child: Container(
+                padding: EdgeInsets.only(left: 5.0, right:5.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RaisedButton(onPressed: _onClickStatus, child: Text('Status: $_status')),
+                      RaisedButton(onPressed: _onClickClear, child: Text('Clear'))
+                    ]
+                )
+            )
+        ),
+      ),
+    );
   }
 
   checkGps() async {
@@ -77,14 +307,16 @@ class _MyLoginPageState extends State<MyLoginPage> {
       locations = null;
     } else {
       setState(() => trackLocation = true);
-      streamSubscription = Geolocation.locationUpdates(
+      streamSubscription = Geolocation.currentLocation(
         accuracy: LocationAccuracy.best,
-        displacementFilter: 0.0,
         inBackground: false,
-      ).listen((result) {
+      ).listen((result) async {
         final location = result;
         setState(() {
           locations = location;
+          if(locations != null) {
+            apiRequest();
+          }
         });
       });
       streamSubscription.onDone(() => setState(() {
@@ -92,6 +324,35 @@ class _MyLoginPageState extends State<MyLoginPage> {
       }));
     }
   }
+
+  Future<String> apiRequest() async {
+    var now = new DateTime.now();
+    var fecha = new DateFormat('yyyyMMdd');
+    var hora = new DateFormat('HHmm');
+    final item = {
+      "dni" : _dni,
+      "fecha": fecha.format(now) ,
+      "hora": hora.format(now) ,
+      "latitud": locations.location.latitude,
+      "longitud": locations.location.longitude
+    };
+    HttpClient httpClient = HttpClient();
+    HttpClientRequest request = await httpClient.postUrl(Uri.parse('http://coe.jujuy.gob.ar/covid19/tracking'));
+    request.headers.set('content-type', 'application/json');
+    request.add(utf8.encode(json.encode(item)));
+    HttpClientResponse response = await request.close();
+    print(response);
+    print(response.statusCode);
+    if(response.statusCode == 200){
+      String reply = await response.transform(utf8.decoder).join();
+      httpClient.close();
+      return reply;
+    } else {
+      return null;
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -138,10 +399,6 @@ class _MyLoginPageState extends State<MyLoginPage> {
      ),
    );*/
   }
-
-//  Widget crearLocation(){
-//    return locations.location != null ? Text("Latitud: ${locations.location?.latitude}") : Container();
-//  }
 
   Widget build_child() {
     if(_loaded)
@@ -320,25 +577,26 @@ class _MyLoginPageState extends State<MyLoginPage> {
                        ],
                      ),
                    ),
-                   Container(
-                     child: FloatingActionButton(
-                       child: Text('Get locations'),
-                       onPressed: getLocations,
-                     ),
-                   ),
-                   Center(
-                       child: Container(
-                         width: 400.0,
-                         height: 500.0,
-                         child: ListView(
-                           children: [
-                             locations != null ? Text("Latitud: ${locations.location?.latitude}") : Container(),
-                             locations != null ? Text("Longitud: ${locations.location?.longitude}") : Container(),
-                             locations != null ? Text("Altitud: ${locations.location?.altitude}") : Container(),
-                           ],
-                         ),
-                       )
-                   ),
+//                   Container(
+//                     child: FloatingActionButton(
+//                       child: Text('Get locations'),
+//                       onPressed: getLocations,
+//                     ),
+//                   ),
+//                   Center(
+//                       child: Container(
+//                         width: 400.0,
+//                         height: 100.0,
+//                         child: ListView(
+//                           children: [
+//                             locations != null ? Text("Latitud: ${locations.location?.latitude}") : Container(),
+//                             locations != null ? Text("Longitud: ${locations.location?.longitude}") : Container(),
+//                             locations != null ? Text("Altitud: ${locations.location?.altitude}") : Container(),
+//                           ],
+//                         ),
+//                       )
+//                   ),
+//                   buildCron(context)
 
                  ],
                )),
