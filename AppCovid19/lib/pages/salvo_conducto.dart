d5eb5c071b7ce5bb5417e4d43645ff1e52958f47
Juan Manuel Lopez <juan.lopez@valtira.net>
2020-04-08 08:12:22 -0300

    add salvo conducto

---
+++ AppCovid19/lib/pages/salvo_conducto.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SalvoConducto extends StatefulWidget {
+  @override
+  _SalvoConductoState createState() => _SalvoConductoState();
+}
+
+class _SalvoConductoState extends State<SalvoConducto> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
