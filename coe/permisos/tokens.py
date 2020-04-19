36f75521e4b8e2f47e20206bad101770f8cdeebc
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-04-19 16:56:00 -0300

    Se arreglo bug de limite

--- coe/permisos/tokens.py
+++ coe/permisos/tokens.py
@@ -4,4 +4,4 @@ import random
 
 #Definimos nuestras funciones
 def token_ingreso():
-    return ''.join(random.sample(string.ascii_uppercase + string.digits, k=50))
+    return ''.join(random.sample(string.ascii_uppercase + string.digits, k=25))
