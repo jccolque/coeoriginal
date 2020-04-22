ce36c3dbc35b386c4fdf3810c54bf6871723ec50
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-04-22 16:15:57 -0300

    Se genero sistema para recuperar operadores

--- coe/permisos/utilidades/arreglos.py
+++ coe/permisos/utilidades/arreglos.py
@@ -1,9 +1,23 @@
 #Imports Extras
-
+from auditlog.models import LogEntry
 #Imports de la app
+from operadores.models import Operador
 from permisos.models import IngresoProvincia
 
 def recuperar_operadores():
     ingresos = IngresoProvincia.objects.filter(estado='A', operador=None)
     for ingreso in ingresos:
-        
+        print(str(ingreso)+' No tiene Operador.')
+        regs = LogEntry.objects.filter(
+            object_id=ingreso.pk,
+            content_type__model="ingresoprovincia",
+            changes__icontains='operador'
+        )
+        for reg in regs.all():
+            if 'operador' in reg.changes_dict:
+                if 'estado' in reg.changes_dict:
+                    if reg.changes_dict['estado'][1] == 'A':
+                        operador = Operador.objects.get(pk=reg.changes_dict['operador'][1])
+                        print('El que aprobo fue: '+str(operador))
+                        ingreso.operador = operador
+                        ingreso.save()
