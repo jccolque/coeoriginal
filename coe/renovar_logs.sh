06f46131b354012c4b606893c85d8b0fe8b433e4
servidor <mmandrille@gmail.com>
2020-04-27 17:34:16 -0300

    Agregamos localidad al individuo

---
+++ coe/renovar_logs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+d=`date +%m-%d-%Y`
+mv archivos/logs/errors.txt archivos/logs/$d-errors.txt
+touch archivos/logs/errors.txt
+mv archivos/logs/apis.txt archivos/logs/$d-apis.txt
+touch archivos/logs/apis.txt
