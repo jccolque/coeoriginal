323bb7476e00837980643fe47d4dee9c87b6adad
Marcos Federico Mandrille <mmandrile@gmail.com>
2020-04-27 15:50:22 -0300

    Se sigue trabando en mejorar implementacion

--- coe/geotracking/apps.py
+++ coe/geotracking/apps.py
@@ -7,7 +7,3 @@ class GeotrackingConfig(AppConfig):
     name = 'geotracking'
     def ready(self):
         agregar_menu(self)
-        #Tareas recursivas:
-        from .tasks import geotrack_sin_actualizacion, finalizar_geotracking
-        geotrack_sin_actualizacion(repeat=3600)#Cada una hora
-        finalizar_geotracking(repeat=3600*12)#Cada 12 horas
--- coe/informacion/apps.py
+++ coe/informacion/apps.py
@@ -7,6 +7,10 @@ class InformacionConfig(AppConfig):
     name = 'informacion'
     def ready(self):
         agregar_menu(self)
-        #Tareas recursivas:
+        #Informacion:
         from .tasks import baja_seguimiento
         baja_seguimiento(repeat=3600*12)#Cada 12 horas
+        #Geotracking
+        from .tasks import geotrack_sin_actualizacion, finalizar_geotracking
+        geotrack_sin_actualizacion(repeat=3600)#Cada una hora
+        finalizar_geotracking(repeat=3600*12)#Cada 12 horas
