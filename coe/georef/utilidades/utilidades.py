6beeb02e0072e44d7ed927cf5b1af4245fe0a9a0
Marcos Federico Mandrille <mmandrile@gmail.com>
2020-04-30 15:14:47 -0300

    mejoramos actualizador de ocupacion

--- coe/georef/utilidades/utilidades.py
+++ coe/georef/utilidades/utilidades.py
@@ -3,5 +3,8 @@ from georef.models import Ubicacion
 def reparar_ocupacion():
     ubicaciones = Ubicacion.objects.filter(tipo='AI')
     for ubicacion in ubicaciones:
+        print('\n')
+        print(ubicacion.nombre + ' | Capacidad Actual: ' + str(ubicacion.capacidad_ocupada))
         ubicacion.capacidad_ocupada = ubicacion.aislados_actuales().count()
+        print('Actualizado a: ' + str(ubicacion.capacidad_ocupada))
         ubicacion.save()
