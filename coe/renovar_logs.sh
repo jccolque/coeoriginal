fb62ed894aa6a6b9b4fef5aa1ac1f802e75626d8
servidor <mmandrille@gmail.com>
2020-05-12 04:57:36 -0300

    Se mejoro renovar logs

--- coe/renovar_logs.sh
+++ coe/renovar_logs.sh
@@ -6,3 +6,6 @@ mv archivos/logs/apis.txt archivos/logs/$d-apis.txt
 touch archivos/logs/apis.txt
 mv archivos/logs/tasks.txt archivos/logs/$d-tasks.txt
 touch archivos/logs/tasks.txt
+mv archivos/logs/signals.txt archivos/logs/$d-signals.txt
+touch archivos/logs/signals.txt
+
