a32c2fdf789b76f482942a53b18f51bd4b8404e1
Marcos Federico Mandrille <mmandrile@gmail.com>
2020-05-22 15:15:48 -0300

    Se mejoro sistema de menu para mobile

---
+++ coe/static_source/js/menu.js
@@ -0,0 +1,8 @@
+function togle_menu() {
+  var x = document.getElementById("nav-mobile");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
