32617c381782f08d56c80d455fcd37cca2ca5859
pablonicolasr <pablonicolasr777@gmail.com>
2020-05-25 11:59:08 -0300

    ModelAdmin

--- coe/provision/admin.py
+++ coe/provision/admin.py
@@ -1,3 +1,13 @@
+#Imports de Django
 from django.contrib import admin
+#Imports de la app
+from .models import Peticionp
+
+#Definimos Custom Admin
+class PeticionPAdmin(admin.ModelAdmin):
+    model = Peticionp
+    list_filter = ['estado', 'fecha', 'destino']
+    search_fields = ['individuos__apellidos', 'individuos__num_doc']
 
 # Register your models here.
+admin.site.register(Peticionp, PeticionPAdmin)
