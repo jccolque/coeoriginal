e2f7f369ddb23c5758914068e45dab256686318e
pablonicolasr <pablonicolasr777@gmail.com>
2020-05-25 15:18:45 -0300

    urls

--- coe/provision/urls.py
+++ coe/provision/urls.py
@@ -18,4 +18,8 @@ urlpatterns = [
     path('peticion/<str:token>', views.ver_peticion_persona, name='ver_peticion_persona'),
     path('mod/people/<int:peticion_id>/<int:individuo_id>', views.cargar_people, name='mod_people'),
     path('del/peticion/<int:peticion_id>/<int:individuo_id>', views.quitar_persona, name='quitar_persona'),
+    #Ingresos
+    path('', views.menu_provision, name='menu_peticiones'),    
+    path('lista/peticiones/completas', views.lista_peticiones_completas, name='lista_peticiones_completas'),
+    path('lista/peticiones', views.lista_peticiones, name='lista_peticiones'),
 ]
