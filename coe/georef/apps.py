6364ed14353716a82cd928709d7125e70d6a1565
Marcos Federico Mandrille <mmandrile@gmail.com>
2020-06-04 03:42:27 -0300

    hotfix

--- coe/georef/apps.py
+++ coe/georef/apps.py
@@ -8,11 +8,11 @@ class GeorefConfig(AppConfig):
     def ready(self):
         agregar_menu(self)
         #Inicializacion de georeferencias:
-        try:
-            from .models import Nacionalidad
-            if not Nacionalidad.objects.exists():
-                from background.functions import inicializar_background_job
-                from .tasks import obtener_localidades_infra_gob
-                inicializar_background_job(verbose_name='obtener_localidades_infra_gob')
-        except:
-            print("No se pudo inicializar georeferencias")
+        # try:
+        #     from .models import Nacionalidad
+        #     if not Nacionalidad.objects.exists():
+        #         from background.functions import inicializar_background_job
+        #         from .tasks import obtener_localidades_infra_gob
+        #         inicializar_background_job(verbose_name='obtener_localidades_infra_gob')
+        # except:
+        #     print("No se pudo inicializar georeferencias")
