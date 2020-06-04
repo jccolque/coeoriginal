185e0f52a96b54531cc219c4533362a15c5ad304
Marcos Federico Mandrille <mmandrile@gmail.com>
2020-06-04 20:09:16 -0300

    hotfix

--- coe/georef/utilidades/utilidades.py
+++ coe/georef/utilidades/utilidades.py
@@ -4,7 +4,7 @@ import traceback
 #Imports del proyecto
 from core.api import requests_retry_session
 from georef.models import Localidad, Departamento, Provincia
-from georef.models import obtener_argentina
+from georef.functions import obtener_argentina
 
 #Definimos webservices origen:
 def actualizar_infra_gob(filename):
