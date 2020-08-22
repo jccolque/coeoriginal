d8880905e500ac00e3900f43b4f1eaf6772ca0de
Marcos Federico Mandrille <mmandrille@gmail.com>
2020-08-22 20:32:22 -0300

    admin para segs de pablo

--- coe/seguimiento/admin.py
+++ coe/seguimiento/admin.py
@@ -1,3 +1,18 @@
 #Imports de Django
 from django.contrib import admin
+#Imports de la app
+from .models import Muestra
+from .models import DatosGis
 
+#Definimos Custom Admin
+class MuestraAdmin(admin.ModelAdmin):
+    model = Muestra
+    list_filter = ['estado', 'fecha_muestra', 'prioridad']
+    search_fields = ['individuos__apellido', 'individuos__num_doc']
+
+class DatosGisAdmin(admin.ModelAdmin):
+    model = DatosGis
+
+# Register your models here.
+admin.site.register(Muestra, MuestraAdmin)
+admin.site.register(DatosGis, DatosGisAdmin)
